b93cf0735e85a7c636949b237413c2fd9ed46e40
archie0732 <121162902+archie0732@users.noreply.github.com>
2025-11-08 22:03:01 +0800

    aaa

---
+++ codeforces/C. Traffic Light/solve.py
@@ -0,0 +1,20 @@
+t = int(input(""))
+
+while t > 0:
+    t -= 1
+    [a, b] = input("").split()
+    s = input("")
+    n = len(s)
+    s += s
+
+    ans = 0
+
+    goal = []
+    for i in range(len(s)):
+
+        if i < n and s[i] == b:
+            goal.append(i)
+        if s[i] == "g" and len(goal) > 0:
+            ans = max(ans, i - goal.pop(0))
+            goal = []
+    print(ans)
